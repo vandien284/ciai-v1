9b8c61be16f43d74e6a7392d072958fdce5743dd
Dien Le <96457535+vandien284@users.noreply.github.com>
2024-11-12 18:15:03 +0700

    Update page.tsx

--- app/library/[slug]/page.tsx
+++ app/library/[slug]/page.tsx
@@ -309,104 +309,138 @@ const Detail = () => {
     return formattedContent;
   }
 
-  const renderContent = (content: any) => {
-    // const elements = content.split('\n').map((line: any, index: any) => {
+  const renderIframe = (content: string) => {
+    const iframeMatch = content.match(/<iframe src="([^"]+)"[^>]*style="([^"]+)"[^>]*><\/iframe>/);
+      if (iframeMatch) {
+        const styleString = iframeMatch[2];
+        const styleObject = Object.fromEntries(
+          styleString.split(';').filter(Boolean).map((rule: any) => {
+            const [property, value] = rule.split(':').map((str: any) => str.trim());
+            return [property.replace(/-([a-z])/g, (_: any, char: string) => char.toUpperCase()), value];
+          })
+        );
 
-    //   const pTagMatch = line.match(/<p[^>]*>/);
-
-    //   if (pTagMatch) {
-    //     return <div key={index} dangerouslySetInnerHTML={{ __html: line }} />;
-    //   }
-
-    //   const strongTextMatch = line.match(/\*\*(.*?)\*\*/);
-    //   const linkMatch = line.match(/(https?:\/\/[^\s]+\.html)/);
-
-    //   if (strongTextMatch && linkMatch) {
-    //     const strongText = strongTextMatch[1];
-    //     const url = linkMatch[0];
-    //     const textBeforeStrong = line.split(strongTextMatch[0])[0];
-    //     const textAfterStrong = line.split(strongTextMatch[0])[1].split(url)[0];
-    //     const textAfterLink = line.split(url)[1];
-
-    //     return (
-    //       <p key={index}>
-    //         {textBeforeStrong}
-    //         <strong>{strongText}</strong>
-    //         {textAfterStrong}
-    //         <a href={url} target="_blank" >
-    //           {url}
-    //         </a>
-    //         {textAfterLink}
-    //       </p>
-    //     );
-    //   }
-
-    //   if (strongTextMatch) {
-    //     const strongText = strongTextMatch[1];
-    //     const textBefore = line.split(strongTextMatch[0])[0];
-    //     const textAfter = line.split(strongTextMatch[0])[1];
-
-    //     return (
-    //       <p key={index}>
-    //         {textBefore}
-    //         <strong>{strongText}</strong>
-    //         {textAfter}
-    //       </p>
-    //     );
-    //   }
-
-    //   if (linkMatch) {
-    //     const url = linkMatch[0];
-    //     const textBeforeLink = line.split(url)[0];
-    //     const textAfterLink = line.split(url)[1];
-    //     if (textBeforeLink.includes('- [Xem chi tiết]')) {
-    //       return (
-    //         <div key={index}>
-    //           <span>- </span>
-    //           <a href={url} target="_blank">
-    //             Xem chi tiết
-    //           </a>
-    //         </div>
-    //       );
-    //     } else {
-    //       return (
-    //         <div key={index}>
-    //           {textBeforeLink}
-    //           <a href={url} target="_blank">
-    //             {url}
-    //           </a>
-    //           {textAfterLink}
-    //         </div>
-    //       );
-    //     }
-
-    //   }
-
-    //   const iframeMatch = line.match(/<iframe src="([^"]+)"[^>]*style="([^"]+)"[^>]*><\/iframe>/);
-    //   if (iframeMatch) {
-    //     const styleString = iframeMatch[2];
-    //     const styleObject = Object.fromEntries(
-    //       styleString.split(';').filter(Boolean).map((rule: any) => {
-    //         const [property, value] = rule.split(':').map((str: any) => str.trim());
-    //         return [property.replace(/-([a-z])/g, (_: any, char: string) => char.toUpperCase()), value];
-    //       })
-    //     );
-
-    //     return (
-    //       <iframe
-    //         key={index}
-    //         src={iframeMatch[1]}
-    //         style={styleObject}
-    //       />
-    //     );
-    //   }
-
-    //   return <div key={index} dangerouslySetInnerHTML={{ __html: line }} />;
-    // });
-
-    // return elements;
-    return <div dangerouslySetInnerHTML={{ __html: formatContent(content) }} />
+        return (
+          <iframe
+            src={iframeMatch[1]}
+            style={styleObject}
+          />
+        );
+      }
+    return null;
   };
+
+  const renderContent = (content: any) => {
+    const formattedContent = formatContent(content);
+    const parts = formattedContent.split(/<iframe[^>]*>.*?<\/iframe>/);
+    return (
+      <div>
+        <div dangerouslySetInnerHTML={{ __html: parts[0] }} />
+        {renderIframe(formattedContent)}
+        <div dangerouslySetInnerHTML={{ __html: parts[1] }} />
+      </div>
+    );
+  };
+
+  // const renderContent = (content: any) => {
+  //   const elements = content.split('\n').map((line: any, index: any) => {
+
+  //     const pTagMatch = line.match(/<p[^>]*>/);
+
+  //     if (pTagMatch) {
+  //       return <div key={index} dangerouslySetInnerHTML={{ __html: line }} />;
+  //     }
+
+  //     const strongTextMatch = line.match(/\*\*(.*?)\*\*/);
+  //     const linkMatch = line.match(/(https?:\/\/[^\s]+\.html)/);
+
+  //     if (strongTextMatch && linkMatch) {
+  //       const strongText = strongTextMatch[1];
+  //       const url = linkMatch[0];
+  //       const textBeforeStrong = line.split(strongTextMatch[0])[0];
+  //       const textAfterStrong = line.split(strongTextMatch[0])[1].split(url)[0];
+  //       const textAfterLink = line.split(url)[1];
+
+  //       return (
+  //         <p key={index}>
+  //           {textBeforeStrong}
+  //           <strong>{strongText}</strong>
+  //           {textAfterStrong}
+  //           <a href={url} target="_blank" >
+  //             {url}
+  //           </a>
+  //           {textAfterLink}
+  //         </p>
+  //       );
+  //     }
+
+  //     if (strongTextMatch) {
+  //       const strongText = strongTextMatch[1];
+  //       const textBefore = line.split(strongTextMatch[0])[0];
+  //       const textAfter = line.split(strongTextMatch[0])[1];
+
+  //       return (
+  //         <p key={index}>
+  //           {textBefore}
+  //           <strong>{strongText}</strong>
+  //           {textAfter}
+  //         </p>
+  //       );
+  //     }
+
+  //     if (linkMatch) {
+  //       const url = linkMatch[0];
+  //       const textBeforeLink = line.split(url)[0];
+  //       const textAfterLink = line.split(url)[1];
+  //       if (textBeforeLink.includes('- [Xem chi tiết]')) {
+  //         return (
+  //           <div key={index}>
+  //             <span>- </span>
+  //             <a href={url} target="_blank">
+  //               Xem chi tiết
+  //             </a>
+  //           </div>
+  //         );
+  //       } else {
+  //         return (
+  //           <div key={index}>
+  //             {textBeforeLink}
+  //             <a href={url} target="_blank">
+  //               {url}
+  //             </a>
+  //             {textAfterLink}
+  //           </div>
+  //         );
+  //       }
+
+  //     }
+
+  //     const iframeMatch = line.match(/<iframe src="([^"]+)"[^>]*style="([^"]+)"[^>]*><\/iframe>/);
+  //     if (iframeMatch) {
+  //       const styleString = iframeMatch[2];
+  //       const styleObject = Object.fromEntries(
+  //         styleString.split(';').filter(Boolean).map((rule: any) => {
+  //           const [property, value] = rule.split(':').map((str: any) => str.trim());
+  //           return [property.replace(/-([a-z])/g, (_: any, char: string) => char.toUpperCase()), value];
+  //         })
+  //       );
+
+  //       return (
+  //         <iframe
+  //           key={index}
+  //           src={iframeMatch[1]}
+  //           style={styleObject}
+  //         />
+  //       );
+  //     }
+
+  //     return <div key={index} dangerouslySetInnerHTML={{ __html: line }} />;
+  //   });
+
+  //   return elements;
+  //   return <div dangerouslySetInnerHTML={{ __html: formatContent(content) }} />
+  // };
+
 
   const handleUpdateNameMessage = async () => {
     await fetchMessage(nameMessage.current);
